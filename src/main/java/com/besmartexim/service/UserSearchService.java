ff75698650c9eba622770c4c7f43592e932eb4a2
Pabitra Ghosal <pabitraghosal5@gmail.com>
2026-01-06 13:25:14 +0530

    remove optional headers.
    remove optional headers.

--- src/main/java/com/besmartexim/service/UserSearchService.java
+++ src/main/java/com/besmartexim/service/UserSearchService.java
@@ -8,10 +8,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import javax.validation.Valid;
